69d12e5600118abec29c97c2093187640ed70f6b
Nadia Tserpiak <nadiacerpiak@gmail.com>
2020-11-22 20:16:07 +0200

    first commit

--- app.js
+++ app.js
@@ -1,22 +1,29 @@
 
-// let screenSav =  document.getElementById("screen-save"); 
-// screenSav.addEventListener("mouseover", mouseOver);
-// screenSav.addEventListener("mouseout", function()
-// {setTimeout(
-//     function(){
-//         screenSav.style.opacity = "1";
-//     }, 1000)});
+let screenSav =  document.getElementById("screen-save"); 
+screenSav.addEventListener("mouseover", mouseOver);
+screenSav.addEventListener("mouseout", function()
+{setTimeout(
+    function(){
+    document.body.style.visibility = "visible";
+        screenSav.style.opacity = "1";
+        imag.style.display = "block";
+    }, 10000)});
 
-// function mouseOver() {
-//     screenSav.style.opacity = "0";
-// }
- 
-let height = document.documentElement.clientHeight;
-let width = document.documentElement.clientWidth;
+function mouseOver() {
+    screenSav.style.opacity = "0";
+    imag.style.display = "none";
+    document.body.style.visibility = "hidden";
+
+}
+const imag = document.getElementById("img");
+let wim = imag.clientWidth;
+let him = imag.clientHeight;
+
+let height = screenSav.clientHeight -700;
+let width = screenSav.clientWidth - 500;
  
 function randomImage() {
     let fileNames = [
-      "img/img6.jpeg",
       "img/img1.jpeg",
       "img/img2.jpeg",
       "img/img3.jpeg",
@@ -24,7 +31,6 @@ function randomImage() {
       "img/img5.jpeg",
     ];
     let randomIndex = Math.floor(Math.random() * fileNames.length);
-    let imag = document.getElementById("img");
     imag.style.background = 'url(' + fileNames[randomIndex] + ')';
     imag.style.backgroundRepeat="no-repeat";
     imag.style.animationName="appear";
@@ -32,14 +38,10 @@ function randomImage() {
 	let randY = Math.floor((Math.random() * height) + 1);
 	let randX = Math.floor((Math.random() * width) + 1);
   imag.style.transform = `translate(${randX}px, ${randY}px)`;
-  let wim = imag.clientWidth;
-  let him = imag.clientHeight;
-  console.log(wim);
-  
   }
   (function loop() {
-    randomImage();   
-                                      
-    let rand = Math.floor(Math.random() ) + 5000;   
-    setTimeout(loop, rand);                             
+    randomImage();                              
+    // let rand = Math.floor(Math.random() ) + 6000;   
+    setTimeout(loop, 5000);                             
   }()); 
+
